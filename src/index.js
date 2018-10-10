2731b6e9e4633bd3a8e2e1e4437012a6da62ad5c
John Haitas <john@haitas.com>
2018-10-10 18:25:22 -0500

    simplifying client entry to play nicer with `preact-cli-entrypoint`

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import createHistory from 'history/createBrowserHistory';
 import './style';
 import App from './components/app';
 
-let appRoot;
 const getProps = () => {
 		const history = createHistory();
 		return { history };
 	},
-	init = () => {
-		const props = getProps();
-		appRoot = render(h(App, props), document.body, appRoot || document.body.firstElementChild);
-	};
-init();
+	props = getProps(),
+	Entry = () => (<App {...props} />);
+
+export default Entry;
