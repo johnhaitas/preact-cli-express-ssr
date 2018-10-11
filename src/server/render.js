d048bef04eba0bd5cce54a5a2748c1f356e92df8
John Haitas <john@haitas.com>
2018-10-11 10:57:37 -0500

    small correction for server side template app root regex

--- src/server/render.js
+++ src/server/render.js
@@ -6,7 +6,7 @@ const headTag = '<head>';
 export default (template, rootComponentClass, placeholderComponentClass) => {
 	const appPlaceholder = placeholderComponentClass
 		? render(h(placeholderComponentClass))
-		: /(<div id="app"[^>]*>).*?(?=<script)/i;
+		: /<div id="app"[^>]*>.*?(?=<script)/i;
 	return props => {
 		const app = render(h(rootComponentClass, props)),
 			head = '', // you could use something like Helmet here
